fda4c196968753495661187c04f6d503800c5d31
Alvaro Delgado Matud <alvdelga@c3r14s6.42madrid.com>
2024-03-11 16:53:02 +0100

    1subida

---
+++ Libft/alvdelga/ft_memset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvdelga <alvdelga@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 15:07:22 by alvdelga          #+#    #+#             */
+/*   Updated: 2024/03/11 15:31:11 by alvdelga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*s;
+	size_t			i;
+
+	i = 0;
+	s = b;
+	while (i < len)
+	{
+		s[i] = (unsigned char)c;
+		i++;
+	}
+	return (b);
+}
