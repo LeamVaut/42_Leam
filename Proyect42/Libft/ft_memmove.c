90bd9967e4911ec3216575558f387bb8a64a1b6b
LeamVaut <156945121+LeamVaut@users.noreply.github.com>
2024-04-15 19:30:42 +0200

    Update ft_memmove.c

--- Proyect42/Libft/ft_memmove.c
+++ Proyect42/Libft/ft_memmove.c
@@ -19,13 +19,26 @@ void	*ft_memmove(void *str1, const void *str2, size_t n)
 	unsigned char		*p_str1;
 	unsigned const char	*p_str2;
 
-	if (str1 < str2)
-		return (ft_memcpy(str1, str2, n));
 	p_str1 = (unsigned char *)str1;
 	p_str2 = (unsigned const char *)str2;
+
+	if (str1 < str2)
+		return (ft_memcpy(str1, str2, n));
 	if (!n || str1 == str2)
 		return (str1);
 	while (n--)
 		p_str1[n] = p_str2[n];
 	return (str1);
+}
+int main()
+{
+     char str[20];
+     char copia[] = "Hola Mundo";
+   
+   printf("%zu\n", strlen(str));
+   printf("%zu\n", sizeof(str));
+   ft_memmove(str, copia, sizeof(copia));
+   printf("%s\n", str);
+   printf("%zu\n", strlen(str));
+   printf("%zu\n", sizeof(str));
 }
