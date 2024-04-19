a4e2f693c08d9d708bb5145581432c79b0758398
LeamVaut <156945121+LeamVaut@users.noreply.github.com>
2024-04-19 08:48:44 +0200

    Update ft_putnbr_fd.c

--- Proyect42/Libft/ft_putnbr_fd.c
+++ Proyect42/Libft/ft_putnbr_fd.c
@@ -31,3 +31,34 @@ void	ft_putnbr_fd(int n, int fd)
 	else
 		ft_putchar_fd(copia + '0', fd);
 }
+/*int	main(void)
+{
+	int	fd;
+	int	nbr;
+
+	// Abre o crea un archivo llamado "ejemplo.txt"
+	// en modo de escritura (O_WRONLY)
+	// y con la bandera O_CREAT que crea el archivo si no existe.
+	fd = open("ejemplo.txt", O_WRONLY | O_CREAT, 0644);
+	if (fd == -1)
+	{
+		perror("Error al abrir el archivo");
+		return (EXIT_FAILURE);
+	}
+
+	// Llama a la función para escribir el carácter en el archivo
+	nbr = 526;
+	ft_putnbr_fd(nbr, fd);
+
+	// Cierra el archivo
+	if (close(fd) == -1)
+	{
+		perror("Error al cerrar el archivo");
+		return (EXIT_FAILURE);
+	}
+
+	// Mostrar el contenido del archivo
+	system("cat ejemplo.txt");
+
+	return (0);
+}*/
