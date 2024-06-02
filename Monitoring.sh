072c01b367d2cbe55ce959b11ad50678c1affeef
LeamVaut <156945121+LeamVaut@users.noreply.github.com>
2024-06-02 17:15:01 +0200

    Update Monitoring.sh

--- Monitoring.sh
+++ Monitoring.sh
@@ -1,11 +1,12 @@
  #!/bin/bash
  
 #Colours
+yellowColour="\e[0;33m\033[1m"
+redColour="\e[0;31m\033[1m"
+
 greenColour="\e[0;32m\033[1m"
 endColour="\033[0m\e[0m"
-redColour="\e[0;31m\033[1m"
 blueColour="\e[0;34m\033[1m"
-yellowColour="\e[0;33m\033[1m"
 purpleColour="\e[0;35m\033[1m"
 turquoiseColour="\e[0;36m\033[1m"
 grayColour="\e[0;37m\033[1m"
@@ -14,7 +15,7 @@ trap ctrl_c SIGINT
 
 function ctrl_c(){
 
-	echo -e "\n${yellowColour}[+]${endColour}${grayColour} Saliendo...${endColour}"; sleep 2
+	echo -e "\n${yellowColour}[+]${endColour}${redColour} Saliendo...${endColour}"; sleep 2
 }
 
  # Arquitectura y versión del kernel
